56d2085f86e19353fb0f48bce46a152c599588ef
BVSh <behzad.valipour@swisstph.ch>
2021-04-21 13:30:47 +0200

    Update the version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="pochas-geoutils",
-    version="0.3.5",
+    version="0.3.6",
     description="A Python package includes geo-utils for PoCHAS project",
     long_description=readme(),
     long_description_content_type="text/markdown",
