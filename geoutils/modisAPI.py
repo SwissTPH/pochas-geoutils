fe0d89205c20de64668c2e4f7de2f9ad7d0c76cd
BVSh <behzad.valipour@swisstph.ch>
2021-04-24 11:48:09 +0200

    Version: 0.4.1:bugs fixed

--- geoutils/modisAPI.py
+++ geoutils/modisAPI.py
@@ -5,7 +5,7 @@
 # https://github.com/ornldaac/modis-viirs-rest-api-python
 
 # For requests
-import requests, json, os
+import requests, json, os,time
 import numpy as np
 from . import grid as gr
 from . import utils as ut
@@ -17,6 +17,9 @@ import rioxarray
 # Utilities
 import argparse
 import sys
+
+
+
 
 
 def main():
@@ -32,17 +35,14 @@ def main():
     parser.add_argument("--startDate", help="Name of data layer. (YYYY-MM-DD') ", type=str)
     parser.add_argument("--endDate", help="Name of data layer. (YYYY-MM-DD') ", type=str)
     parser.add_argument("--path_aoi", help="Path for AOI geojson. ", type=str)
-    parser.add_argument("--crs_aoi", help="CRS for AOI geojson. Default: 4326 ", type=str, default="4326")
-    parser.add_argument("--ouput_crs", help="CRS for output. Default: 4326 ", type=str, default="4326")
-    parser.add_argument("--ouput_cellsize", help="out put image cellsize. Default: 250 ", type=str, default="250")
+    parser.add_argument("--crs_aoi", help="CRS for AOI geojson. Default: 4326 ", type=int, default=4326)
+    parser.add_argument("--ouput_crs", help="CRS for output. Default: 4326 ", type=int, default=4326)
+    parser.add_argument("--ouput_cellsize", help="out put image cellsize. Default: 250 ", type=int, default=250)
 
     args = parser.parse_args()
     satellite = args.satellite
     product = args.product
     band = args.band
-
-
-
 
     # Set subset parameters:
     url = "https://modis.ornl.gov/rst/api/v1/"
@@ -50,16 +50,19 @@ def main():
 
     # list of available products.
     if args.satellite:
+        # print(url + f'products?sensor={satellite}')
         resp = requests.get(url + f'/products?sensor={satellite}', headers=header)
         content = resp.json()
+        time.sleep(5)
         for each in content['products']:
-            print('%s: %s ' % (each['product'], each['description']))
+           print('%s: %s ' % (each['product'], each['description']))
 
     #  retrieve available band names for a product
     if args.product:
         if band is None:
             resp = requests.get(url + f'{product}/bands', headers=header)
             content = resp.json()
+            time.sleep(5)
             for each in content['bands']:
                 print('%s: %s ' % (each['band'], each['description']))
         else:
@@ -82,6 +85,7 @@ def main():
             if endDate is None:
                 print('Please enter End Date')
                 sys.exit()
+
             ouput_crs = args.ouput_crs
             ouput_cellsize = args.ouput_cellsize
 
@@ -103,21 +107,42 @@ def main():
 
             sd = dt.datetime.strptime(startDate, "%Y-%m-%d").date()
             ed = dt.datetime.strptime(endDate, "%Y-%m-%d").date()
-            ab, lr = 50, 50
+            ab, lr = 70, 70
 
             length = (UTMxMax - UTMxMin)
             width = (UTMyMax - UTMyMin)
             area = (length * width)/1000000
+            print(f'Area is equal to: {area} m2')
             if area < 50:
-                cell_size = max(np.floor(length), np.floor(width))
+                print(f'The number of the tiles are: 1')
                 point_long = UTMxMin + length / 2
                 point_lat = UTMyMin + width/2
-                datesurl = [url + f'{product}/dates?latitude={point_lat}&longitude={point_long}']
+                coords=np.asarray(Projection(point_long, point_lat, inverse=True)).T
+                datesurl = [url + f'{product}/dates?latitude={coords[1]}&longitude={coords[0]}']
+
+                responses = [requests.get(date, headers=header) for date in datesurl]
+                time.sleep(2)
+                dates = [json.loads(resp.text)['dates'] for resp in responses]
+                modis_dates = [d['modis_date'] for date in dates for d in date if all([dt.datetime.strptime(d['calendar_date'], "%Y-%m-%d").date() >= sd, dt.datetime.strptime(d['calendar_date'], "%Y-%m-%d").date() < ed])]
+
+                chunks = list(ut.chunk(modis_dates, 10))
+                subsets = []
+                for i, c in enumerate(chunks):
+                    print("[ " + str(i + 1) + " / " + str(len(chunks)) + " ] " + c[0] + " - " + c[-1])
+                    _url = ut.getSubsetURL(url,product, coords[1], coords[0], band, c[0], c[-1], ab, lr)
+                    time.sleep(2)
+                    _response = requests.get(_url, headers=header)
+                    subsets.append(json.loads(_response.text))
+                ut.convert_to_NetCDF(subsets, coords, ouput_crs, ouput_cellsize)
+
             else:
                 grid_UTM = gr.grid(UTMxMin, UTMxMax, UTMyMin, UTMyMax, cell_size=100000, crs=EPSG).generate_grid()
                 point_list_UTM = grid_UTM.centroid
                 point_list_WGS = np.asarray(Projection(point_list_UTM.x.values, point_list_UTM.y.values, inverse=True)).T
+                print(f'The number of the tiles are: {len(point_list_WGS)}')
                 datesurl = [url + f'{product}/dates?latitude={coord[0]}&longitude={coord[1]}' for coord in point_list_WGS]
+
+
                 responses = [requests.get(date, headers=header) for date in datesurl]
                 dates = [json.loads(resp.text)['dates'] for resp in responses]
                 modis_dates = [
@@ -137,42 +162,12 @@ def main():
                         _url = ut.getSubsetURL(url,product, coords[1], coords[0], band, c[0], c[-1], ab, lr)
                         _response = requests.get(_url, headers=header)
                         subsets.append(json.loads(_response.text))
+                    ut.convert_to_NetCDF(subsets, coords, ouput_crs, ouput_cellsize)
 
-                    # Use dictionary comprehension to get some spatial metadata from the first subset in our list:
-                    meta = {key: value for key, value in subsets[0].items() if key != "subset"}
-                    # Iterate over the list of subsets and collect the data in a dictionary:
-                    data = {'dates': [], 'arrays': []}
-                    for i in subsets:
-                        for j in i['subset']:
-                            data['dates'].append(j['calendar_date'])
-                            data['arrays'].append(np.array(j['data']).reshape(meta['nrows'], meta['ncols']))
-
-                    # Use the metadata to make lists of x and y coordinates:
-                    # f(ncols,nrows): n * cellsize + corner_coordinate
-                    dtdates = [dt.datetime.strptime(d, "%Y-%m-%d") for d in data['dates']]
-                    xcoordinates = [float(meta['xllcorner'])] + [i * meta['cellsize'] + float(meta['xllcorner']) for i in
-                                                                 range(1, meta['ncols'])]
-                    ycoordinates = [float(meta['yllcorner'])] + [i * meta['cellsize'] + float(meta['yllcorner']) for i in
-                                                                 range(1, meta['nrows'])]
-                    # Make an xarray.DataArray object:
-
-                    xrDataArray = xr.DataArray(
-                        name=meta['band'],
-                        data=np.flipud(np.dstack(data['arrays'])),
-                        coords=[np.array(ycoordinates), np.array(xcoordinates), dtdates],
-                        dims=["y", "x", "time"],
-                        attrs=dict(units=meta['units'])
-                    )
-                    # Finally, save as netCDF:
-                    xrDataArray_T = xrDataArray.transpose("time", "y", "x")
-                    crs = CRS.from_proj4("+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m")
-                    xrDataArray_CRS = xrDataArray_T.rio.write_crs(crs)
-
-                    if ouput_crs != 4326:
-                        if ouput_cellsize is None:
-                            raise RuntimeError(f"Please determine the cell size for projection")
-                        else:
-                            file = xrDataArray_CRS.rio.reproject(f"epsg:{ouput_crs}", resolution=ouput_cellsize)
-                    file.to_netcdf(f'output_{coords[0]}_{coords[1]}.nc',unlimited_dims="time",engine='netcdf4')
 if __name__ == "__main__":
     main()
+
+
+
+
+
--- geoutils/utils.py
+++ geoutils/utils.py
@@ -1,5 +1,11 @@
 import json,os
+from pyproj import Proj, CRS
+import datetime as dt
+import xarray as xr
+import rioxarray
+import numpy as np
 
+# ModisAPI.py utils:
 def geometry_from_geojson(filepath):
     with open(filepath, 'r') as f:
         json_obj = json.load(f)
@@ -42,6 +48,54 @@ def getSubsetURL( url, prod , lat , lon , band, sd , ed , ab , lr ): return( "".
     "&endDate=", str(ed),
     "&kmAboveBelow=", str(ab),
     "&kmLeftRight=", str(lr) ]) )
+
+
+def convert_to_NetCDF(subsets,coords,ouput_crs,ouput_cellsize):
+    # Use dictionary comprehension to get some spatial metadata from the first subset in our list:
+    meta = {key: value for key, value in subsets[0].items() if key != "subset"}
+    # Iterate over the list of subsets and collect the data in a dictionary:
+    data = {'dates': [], 'arrays': []}
+    for i in subsets:
+        for j in i['subset']:
+            data['dates'].append(j['calendar_date'])
+            data['arrays'].append(np.array(j['data']).reshape(meta['nrows'], meta['ncols']))
+
+    # Use the metadata to make lists of x and y coordinates:
+    # f(ncols,nrows): n * cellsize + corner_coordinate
+    dtdates = [dt.datetime.strptime(d, "%Y-%m-%d") for d in data['dates']]
+    xcoordinates = [float(meta['xllcorner'])] + [i * meta['cellsize'] + float(meta['xllcorner']) for i in
+                                                 range(1, meta['ncols'])]
+    ycoordinates = [float(meta['yllcorner'])] + [i * meta['cellsize'] + float(meta['yllcorner']) for i in
+                                                 range(1, meta['nrows'])]
+    # Make an xarray.DataArray object:
+
+    xrDataArray = xr.DataArray(
+        name=meta['band'],
+        data=np.flipud(np.dstack(data['arrays'])),
+        coords=[np.array(ycoordinates), np.array(xcoordinates), dtdates],
+        dims=["y", "x", "time"],
+        attrs=dict(units=meta['units'])
+    )
+    # Finally, save as netCDF:
+    xrDataArray_T = xrDataArray.transpose("time", "y", "x")
+    crs = CRS.from_proj4("+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m")
+    xrDataArray_CRS = xrDataArray_T.rio.write_crs(crs)
+
+    if ouput_crs != 4326:
+        if ouput_cellsize is None:
+            raise RuntimeError(f"Please determine the cell size for projection")
+        else:
+
+            file = xrDataArray_CRS.rio.reproject(f"epsg:{ouput_crs}", resolution=ouput_cellsize)
+    else:
+        file = xrDataArray_CRS.rio.reproject(f"epsg:{ouput_crs}")
+
+    file.to_netcdf(f'output_{coords[0]}_{coords[1]}.nc', unlimited_dims="time", engine='netcdf4')
+
+
+
+
+
 
 
 def list_files_with_absolute_paths(dirpath,endswith=None):
