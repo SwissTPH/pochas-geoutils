696de78c81b74b2e0fd0c5b1a4480a9cd5536dbc
BVSh <behzad.valipour@swisstph.ch>
2021-04-26 13:02:46 +0200

    Version:0.4.1:bugs fixed:jsonRequest-reduce sleepTime-ChunkNumber

--- geoutils/modisAPI.py
+++ geoutils/modisAPI.py
@@ -36,8 +36,8 @@ def main():
     parser.add_argument("--crs_aoi", help="CRS for AOI geojson. Default: 4326 ", type=int, default=4326)
     parser.add_argument("--ouput_crs", help="CRS for output. Default: 4326 ", type=int, default=4326)
     parser.add_argument("--ouput_cellsize", help="out put image cellsize. Default: 250 ", type=int, default=250)
-    parser.add_argument("--number_chunks", help="There is a limit of a maximum ten modis dates per reques. Default: 5 ",
-                        type=int, default=5)
+    parser.add_argument("--number_chunks", help="There is a limit of a maximum ten modis dates per reques. Default: 10 ",
+                        type=int, default=10)
 
     args = parser.parse_args()
     satellite = args.satellite
