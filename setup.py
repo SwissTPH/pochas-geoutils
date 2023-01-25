0501b4e9ba1ed37471f6b411c1732cafe2a53a6d
Behzad Valipour Sh <47195556+behzad89@users.noreply.github.com>
2023-01-25 14:10:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name="pochas-geoutils",
-    version="0.0.1",
+    version="0.0.3",
     description="A Python package includes geo-utils for PoCHAS project",
     long_description=readme(),
     long_description_content_type="text/markdown",
